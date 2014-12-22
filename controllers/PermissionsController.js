6635bd7d1f4a67792d9f81992cad7a9d04d1efb4
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-22 13:32:29 -0600

    testing...

--- controllers/PermissionsController.js
+++ controllers/PermissionsController.js
@@ -1,5 +1,31 @@
 var getPermissions = function(req, res){
-	res.send({success:true});
+
+	var permissionsObject = {
+		applications: [
+			bradyWorkstation: {
+				appRead: true,
+				appWrite: true,
+				appDelete: true,
+				settingsRead: true,
+				settingsWrite: true,
+				settingsDelete: true,
+				activateLicense: true,
+				print: true
+			},
+			newBradyApp: {
+				usersRead: true,
+				usersWrite: true,
+				usersDelete: true,
+				devicesRead: true,
+				devicesWrite: true,
+				devicesDelete: true,
+				print: true
+			}
+		]
+	};
+
+	res.send(permissionsObject);
+	
 };
 
 module.exports = {
