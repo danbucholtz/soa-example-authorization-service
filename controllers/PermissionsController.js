22cb461ea38328d7837b6c928f53ebb4c7fc0e72
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-23 14:12:35 -0600

    dunno - some sort of update

--- controllers/PermissionsController.js
+++ controllers/PermissionsController.js
@@ -2,24 +2,28 @@ var getPermissions = function(req, res){
 
 	var permissionsObject = {
 		applications: [
-			bradyWorkstation: {
-				appRead: true,
-				appWrite: true,
-				appDelete: true,
-				settingsRead: true,
-				settingsWrite: true,
-				settingsDelete: true,
-				activateLicense: true,
-				print: true
+			{
+				bradyWorkstation: {
+					appRead: true,
+					appWrite: true,
+					appDelete: true,
+					settingsRead: true,
+					settingsWrite: true,
+					settingsDelete: true,
+					activateLicense: true,
+					print: true
+				}
 			},
-			newBradyApp: {
-				usersRead: true,
-				usersWrite: true,
-				usersDelete: true,
-				devicesRead: true,
-				devicesWrite: true,
-				devicesDelete: true,
-				print: true
+			{
+				newBradyApp: {
+					usersRead: true,
+					usersWrite: true,
+					usersDelete: true,
+					devicesRead: true,
+					devicesWrite: true,
+					devicesDelete: true,
+					print: true
+				}
 			}
 		]
 	};
